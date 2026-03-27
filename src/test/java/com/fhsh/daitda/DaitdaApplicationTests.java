76e83ee86549fec3e7dfe31e7d543af324f26b27
hil679 <hil679922@gmail.com>
2026-03-27 12:45:49 +0900

    Init: 프로젝트 초기 설정

---
+++ src/test/java/com/fhsh/daitda/DaitdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhsh.daitda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaitdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

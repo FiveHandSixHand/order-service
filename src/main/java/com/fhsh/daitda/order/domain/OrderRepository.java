0c9544e91a1a1bcf43603af4439c00d18294c5b7
hil679 <hil679922@gmail.com>
2026-03-31 03:10:57 +0900

    Feat: OrderRepository 저장 및 단건 조회 메서드 추가 - OrderRepository 인터페이스 내 save, findById 정의

--- src/main/java/com/fhsh/daitda/order/domain/OrderRepository.java
+++ src/main/java/com/fhsh/daitda/order/domain/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.fhsh.daitda.order.domain;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface OrderRepository {
+	public Order save(Order order);
+	public Optional<Order> findById(UUID orderId);
 }
--- src/main/java/com/fhsh/daitda/order/infrastructure/OrderRepositoryImpl.java
+++ src/main/java/com/fhsh/daitda/order/infrastructure/OrderRepositoryImpl.java
@@ -1,7 +1,11 @@
 package com.fhsh.daitda.order.infrastructure;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.stereotype.Repository;
 
+import com.fhsh.daitda.order.domain.Order;
 import com.fhsh.daitda.order.domain.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +14,14 @@ import lombok.RequiredArgsConstructor;
 @Repository
 public class OrderRepositoryImpl implements OrderRepository {
 	private final OrderJpaRepository orderJpaRepository;
+
+	@Override
+	public Order save(Order order) {
+		return orderJpaRepository.save(order);
+	}
+
+	@Override
+	public Optional<Order> findById(UUID orderId) {
+		return orderJpaRepository.findById(orderId);
+	}
 }
